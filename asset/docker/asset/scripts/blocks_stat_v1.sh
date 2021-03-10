f20fec876ff0bf10eaabcd204e8265543786913a
Mobin Asri <masri@ucsc.edu>
2021-03-10 06:30:29 -0800

    Sort bed files before merging

--- asset/docker/asset/scripts/blocks_stat_v1.sh
+++ asset/docker/asset/scripts/blocks_stat_v1.sh
@@ -26,19 +26,19 @@ printf "\n#######\nBIONANO\n#######\n"
 cat $prefix.bionano.low_high.trim1k.no_censat.gt10.bed | python3 ${BLOCK_STATS_PY}
 
 printf "\n############\nHIFI AND ONT\n############\n"
-bedtools intersect -a $prefix.hifi.low_high.trim1k.gt10.bed -b $prefix.ont.low_high.trim1k.gt10.bed | bedtools merge -i - | python3 ${BLOCK_STATS_PY}
+bedtools intersect -a $prefix.hifi.low_high.trim1k.gt10.bed -b $prefix.ont.low_high.trim1k.gt10.bed | bedtools sort -i - | bedtools merge -i - | python3 ${BLOCK_STATS_PY}
 
 #printf "\n###############\nHIC AND BIONANO\n###############\n"
 #bedtools intersect -a $prefix.hic.low_high.trim1k.no_censat.gt10.bed -b $prefix.bionano.low_high.trim1k.no_censat.gt10.bed > $prefix.hic_and_bionano.low_high.trim1k.no_censat.gt10.bed
 #cat $prefix.hic_and_bionano.low_high.trim1k.no_censat.gt10.bed | python3 ${BLOCK_STATS_PY}
 
 printf "\n########################\nHIFI AND BIONANO\n########################\n"
-bedtools intersect -a $prefix.hifi.low_high.trim1k.gt10.bed -b $prefix.bionano.low_high.trim1k.no_censat.gt10.bed | bedtools merge -i - | python3 ${BLOCK_STATS_PY}
+bedtools intersect -a $prefix.hifi.low_high.trim1k.gt10.bed -b $prefix.bionano.low_high.trim1k.no_censat.gt10.bed | bedtools sort -i - | bedtools merge -i - | python3 ${BLOCK_STATS_PY}
 
 printf "\n#######################\nONT AND BIONANO\n#######################\n"
-bedtools intersect -a $prefix.ont.low_high.trim1k.gt10.bed -b $prefix.bionano.low_high.trim1k.no_censat.gt10.bed | bedtools merge -i - | python3 ${BLOCK_STATS_PY}
+bedtools intersect -a $prefix.ont.low_high.trim1k.gt10.bed -b $prefix.bionano.low_high.trim1k.no_censat.gt10.bed | bedtools sort -i - | bedtools merge -i - | python3 ${BLOCK_STATS_PY}
 
 printf "\n#######################\nHiFi and ONT AND BIONANO\n#######################\n"
-bedtools intersect -a $prefix.ont.low_high.trim1k.gt10.bed -b $prefix.hifi.low_high.trim1k.gt10.bed | bedtools merge -i - > $prefix.ont_and_hifi.low_high.trim1k.gt10.bed
-bedtools intersect -a $prefix.ont_and_hifi.low_high.trim1k.gt10.bed -b $prefix.bionano.low_high.trim1k.no_censat.gt10.bed | bedtools merge -i - | python3 ${BLOCK_STATS_PY}
+bedtools intersect -a $prefix.ont.low_high.trim1k.gt10.bed -b $prefix.hifi.low_high.trim1k.gt10.bed | bedtools sort -i - | bedtools merge -i - > $prefix.ont_and_hifi.low_high.trim1k.gt10.bed
+bedtools intersect -a $prefix.ont_and_hifi.low_high.trim1k.gt10.bed -b $prefix.bionano.low_high.trim1k.no_censat.gt10.bed | bedtools sort -i -| bedtools merge -i - | python3 ${BLOCK_STATS_PY}
 
