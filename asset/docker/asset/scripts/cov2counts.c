bb2f11db52065fc020a620bfd893f2e4002290fc
Mobin Asri <masri@ucsc.edu>
2021-03-27 22:49:26 +0000

    Added C code and its binary file for faster coverting of coverage to counts

---
+++ asset/docker/asset/scripts/cov2counts.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+
+void cov2counts(char* inPath, char* outPath){
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char * token;
+    int start=0, end=0, cov=0;
+    int maxCov = 1000;
+    int* counts = malloc( (maxCov + 1) * sizeof(int));
+    // initialize counts
+    for(int i = 0; i <= maxCov; i++){
+	    counts[i] = 0;
+    }
+    fp = fopen(inPath, "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+    while ((read = getline(&line, &len, fp)) != -1) {
+        if (line[0] == '>') continue;
+	line[strlen(line)-1] = '\0';
+	//printf("%s\n", line);
+	// get start location
+	token = strtok(line, "\t");
+	start = atoi(token);
+	// get end location
+        token = strtok(NULL, "\t");
+	end = atoi(token);
+	// get coverage
+	token = strtok(NULL, "\t");
+        cov = atoi(token);
+	if (cov > maxCov) {
+		counts = (int*) realloc(counts, (cov + 1) * sizeof(int));
+		// initialize just added elements
+		for(int i = (maxCov + 1); i <= cov; i++){
+			counts[i] = 0;
+		}
+		maxCov = cov;
+	}	
+	counts[cov] += end - start + 1;
+    }
+    fclose(fp);
+    // write to output
+    fp = fopen(outPath, "w+");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+    for(int i = 0; i <= maxCov; i++){
+            fprintf(fp, "%d\t%d\n", i, counts[i]);
+    }
+    fclose(fp);
+}
+int main(int argc, char *argv[]) {
+   int c;
+   char* inputPath;
+   char* outputPath;
+   char *program;
+   (program = strrchr(argv[0], '/')) ? ++program : (program = argv[0]);
+   while (~(c=getopt(argc, argv, "i:o:h"))) {
+		switch (c) {
+			case 'i':
+				inputPath = optarg; 
+				break;
+			case 'o':
+				outputPath = optarg; 
+				break;
+			default:
+				if (c != 'h') fprintf(stderr, "[E::%s] undefined option %c\n", __func__, c);
+help:	
+				fprintf(stderr, "\nUsage: %s  -i -i <INPUT> -o <OUTPUT> \n", program);
+				fprintf(stderr, "Options:\n");	
+				fprintf(stderr, "         -i         input .cov file\n");	
+				fprintf(stderr, "         -o         output .counts file\n");
+				return 1;	
+		}		
+   }
+   cov2counts(inputPath, outputPath);
+   return 0;
+}
