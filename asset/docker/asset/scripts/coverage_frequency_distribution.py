766ce9b069a2fdf885eb82e26d805a2696b123c2
Mobin Asri <masri@ucsc.edu>
2021-03-28 22:32:06 +0000

    Updated the coverage model

--- asset/docker/asset/scripts/coverage_frequency_distribution.py
+++ asset/docker/asset/scripts/coverage_frequency_distribution.py
@@ -8,16 +8,58 @@ Created on Sun Jan 24 14:29:21 2021
 
 # Some statistics helper functions
 from copy import deepcopy
-from math import exp, sqrt, pi
+from math import log, exp, sqrt, pi, ceil
 def exp_pdf(x, lam):
     if x < 0.0:
         return 0.0
     else:
         return lam * exp(-lam * x)   
+def trunc_exp_pdf(x, lam, b):
+    if x < 0.0 or x > b:
+        return 0.0
+    else:
+        return exp_pdf(x, lam) / (1.0 - exp(-lam * b)) 
 def norm_pdf(x, mu, sigma):
     z = (x - mu) / sigma
     return 1.0 / (sigma * sqrt(2.0 * pi)) * exp(-z * z / 2.0)
 
+# adapted from https://en.wikipedia.org/wiki/Golden-section_search
+def golden_section_search(f, a, b, tol = 1e-6):
+    
+    invphi = (sqrt(5.0) - 1.0) / 2.0  # 1 / phi
+    invphi2 = (3.0 - sqrt(5.0)) / 2.0 # 1 / phi^2
+    
+    h = b - a
+    if h <= tol:
+        return (b + a) / 2.0
+    
+    n = int(ceil(log(tol / h) / log(invphi)))
+    
+    c = a + invphi2 * h
+    d = a + invphi * h
+    yc = f(c)
+    yd = f(d)
+    
+    for k in range(n-1):
+        if yc > yd:
+            b = d
+            d = c
+            yd = yc
+            h = invphi * h
+            c = a + invphi2 * h
+            yc = f(c)
+        else:
+            a = c
+            c = d
+            yc = yd
+            h = invphi * h
+            d = a + invphi * h
+            yd = f(d)
+    
+    if yc > yd:
+        return (a + d) / 2.0
+    else:
+        return (c + b) / 2.0
 
 ##
 # A class that represents a model of coverage frequencies in a phased diploid
@@ -36,7 +78,8 @@ class CoverageDistribution:
         return self.component_probabilities(c)[0]
     
     # Return the probability that an element in the assembly with coverage c is
-    # present in the underlying genome and haploid (indicating a)
+    # present in the underlying genome and haploid (indicating that is it
+    # correctly assembled)
     def probability_haploid(self, c):
         return self.component_probabilities(c)[1]
     
@@ -45,7 +88,8 @@ class CoverageDistribution:
     # (note that it may be that another copy of this element is where the 
     # collapsing actually ocurred)
     def probability_collapsed(self, c):
-        return sum(self.component_probabilities(c)[2:])
+        comps = self.component_probabilities(c)
+        return 1 - comps[0] - comps[1]
     
     # Fit and return a CoverageDistribution using a coverage frequency histogram,
     # which should be provided as either a collections.Counter or dict whose keys
@@ -57,35 +101,45 @@ class CoverageDistribution:
         # copy the spectrum so we can modify it without breaking expectations
         spectrum = deepcopy(spectrum)
         
-        # This is only necessary with KMC3 output
-        # zero out max_val to account for binning at the max value
-        #spectrum[max(spectrum)] = 0
-        
         # get a coarse estimate that we can use as a starting point for EM
         cov_dist = CoverageDistribution.heuristic_fit(spectrum)
         
         for iteration in range(max_iters):
-            print("iter {}: cov={}, var={}, scale={}, weights={}".format(iteration, cov_dist.cov_per_ploidy, sqrt(cov_dist.var_per_ploidy),cov_dist.err_scale, cov_dist.mixture_weights[:3]))
+            
+#            print(f"iteration {iteration}")
+#            print("\terr scale {}".format(cov_dist.err_scale))
+#            print("\tcov per ploidy {}".format(cov_dist.cov_per_ploidy))
+#            print("\tvar per ploidy {}".format(cov_dist.var_per_ploidy))
+#            print("\tlow ploidy weights {}".format(", ".join("{:.3e}".format(v) for v in cov_dist.mixture_weights[:5])))
+#            print("\thigh ploidy weights {}".format(", ".join("{:.3e}".format(v) for v in cov_dist.mixture_weights[-8:])))
+#            c = sorted(spectrum)[:200]
+#            plt.plot(c, [cov_dist.pdf(x) for x in c])
+#            plt.show()
+            
             assignment = {}
             for cov in spectrum:
-                assignment[cov] = cov_dist.component_probabilities(cov)
+                assignment[cov] = cov_dist.component_probabilities(cov_dist.cov_adj(cov))
         
             new_mixture_weights = []
             
-            # error scale is the weighted mean
-            numer = 0.0
-            denom = 0.0
+            weight_denom = sum(spectrum.values())
+                        
+            # gather sufficient statistics for error scale
+            eff_N = 0.0
+            eff_sum = 0.0
             weight_numer = 0.0
-            weight_denom = 0.0
             for cov in spectrum:
                 freq = spectrum[cov]
                 prob = assignment[cov][0]
-                numer += freq * prob * cov
-                denom += freq * prob
+                eff_sum += freq * prob * cov_dist.cov_adj(cov)
+                eff_N += freq * prob
                 weight_numer += prob * freq
-                weight_denom += freq
-                
-            new_error_scale = numer / denom
+                          
+            # maximize likelihood for a truncated exponential
+            b = cov_dist.err_trunc_point()
+            llike = lambda lam: eff_N * log(lam) - eff_N * log(1.0 - exp(-lam * b)) - eff_sum * lam
+            rate = golden_section_search(llike, 0, b, tol / 100.0)
+            new_error_scale = 1.0 / rate
             
             # component weight is weighted proportion assigned to the error component
             new_mixture_weights.append(weight_numer / weight_denom)
@@ -98,14 +152,13 @@ class CoverageDistribution:
                 for cov in spectrum:
                     freq = spectrum[cov]
                     prob = assignment[cov][ploidy]
-                    numer += freq * prob * cov
+                    numer += freq * prob * cov_dist.cov_adj(cov)
                     denom += prob * freq * ploidy
                     weight_numer += prob * freq
                 new_mixture_weights.append(weight_numer / weight_denom)
                 
             new_cov_per_ploidy = numer / denom
             
-            #print("doing std dev")
             # the haploid std deviation is the sqrt of the weighted average
             # square deviation, adjusted for the fact that the variance of
             # ploidy P should be P times the variance of the haploid
@@ -115,25 +168,17 @@ class CoverageDistribution:
                 freq = spectrum[cov]
                 for ploidy in range(1, cov_dist.max_ploidy() + 1):
                     prob = assignment[cov][ploidy]
-                    dev = cov - ploidy * new_cov_per_ploidy
+                    dev = cov_dist.cov_adj(cov) - ploidy * new_cov_per_ploidy
                     numer += freq * prob * dev * dev / ploidy
                     denom += freq * prob
                     
             new_var_per_ploidy = numer / denom
             
-            # Limiting the error scale to an expected interval only when 
-            # we have a reasonable numebr of zero coverages
-            if spectrum[0] > spectrum[1]:
-                expected_error_scale =  sqrt(new_var_per_ploidy * 2.0 * pi) * (spectrum[round(new_cov_per_ploidy)] / spectrum[0]) * (new_mixture_weights[0] / new_mixture_weights[1])
-                epsilon = abs(new_error_scale - expected_error_scale) / expected_error_scale
-                if epsilon > 0.1:
-                    new_error_scale = expected_error_scale
-
             # the next iteration's distrubition
             new_cov_dist = CoverageDistribution(new_cov_per_ploidy, new_var_per_ploidy,
-                                                new_error_scale, new_mixture_weights)
+                                                new_error_scale, new_mixture_weights,
+                                                cov_dist.err_trunc_point())
             
-            #print("doing convergence")
             converged = cov_dist.converged(new_cov_dist, tol)
             cov_dist = new_cov_dist
             if converged:
@@ -156,8 +201,9 @@ class CoverageDistribution:
         max_frequency = -1
         has_increased = False
         for cov_value in sorted(spectrum)[1:]:
-            if spectrum[cov_value] > spectrum[cov_value - 1]:
-                has_increased = True
+            if cov_value - 1 in spectrum:
+                if spectrum[cov_value] > spectrum[cov_value - 1]:
+                    has_increased = True
             if spectrum[cov_value] > max_frequency and has_increased:
                 max_frequency = spectrum[cov_value]
                 cov_at_max_frequency = cov_value
@@ -168,7 +214,7 @@ class CoverageDistribution:
         max_ploidy = int(max_cov / cov_per_ploidy)
         while max_ploidy * cov_per_ploidy - 3.0 * sqrt(max_ploidy * cov_per_ploidy) < max_cov:
             max_ploidy += 1
-            
+        
         # start with an arbitrary, small scale
         err_scale = 1.0
         
@@ -181,16 +227,20 @@ class CoverageDistribution:
         for i in range(max_ploidy + 1):
             count = 1
             for cov in range(int((i - 0.5) * cov_per_ploidy), int((i + 0.5) * cov_per_ploidy)):
-                count += spectrum[cov]
+                if cov in spectrum:
+                    count += spectrum[cov]
             mixture_weights.append(count / total_count)
         
-        return CoverageDistribution(cov_per_ploidy, var_per_ploidy, err_scale, mixture_weights)
+        return CoverageDistribution(cov_per_ploidy, var_per_ploidy, err_scale, mixture_weights, cov_per_ploidy)
     
-    def __init__(self, cov_per_ploidy, var_per_ploidy, err_scale, mixture_weights):
+    def __init__(self, cov_per_ploidy, var_per_ploidy, err_scale, mixture_weights, err_trunc_point, 
+                 pseudo_cov = .25):
         self.cov_per_ploidy = cov_per_ploidy
         self.var_per_ploidy = var_per_ploidy
         self.err_scale = err_scale
         self.mixture_weights = mixture_weights
+        self.err_trunc = err_trunc_point
+        self.pseudo_cov = pseudo_cov
     
     def pdf(self, x):
         return sum(self.likelihood(ploidy, x) for ploidy in range(self.max_ploidy() + 1))
@@ -198,14 +248,20 @@ class CoverageDistribution:
     def max_ploidy(self):
         return len(self.mixture_weights) - 1
     
+    def cov_adj(self, cov):
+        return cov + self.pseudo_cov
+    
     def likelihood(self, ploidy, cov):
         l = 0.0
         if ploidy == 0:
             # error distribution, switch to rate parameterization
-            l = exp_pdf(cov, 1.0 / self.err_scale)
+            l = trunc_exp_pdf(cov, 1.0 / self.err_scale, self.err_trunc_point())
         else:
             l = norm_pdf(cov, self.cov_per_ploidy * ploidy, sqrt(self.var_per_ploidy * ploidy))
         return self.mixture_weights[ploidy] * l
+    
+    def err_trunc_point(self):
+        return self.err_trunc
     
     def component_probabilities(self, cov):
         likelihoods = [self.likelihood(ploidy, cov) for ploidy in range(self.max_ploidy() + 1)]
@@ -221,7 +277,7 @@ class CoverageDistribution:
             return False
         if other.err_scale != 0.0 and abs(self.err_scale / other.err_scale - 1.0) > tol:
             return False
-        max_ploidy_of_interest = 4
+        max_ploidy_of_interest = 2
         for wt, other_wt, ploidy in zip(self.mixture_weights, other.mixture_weights, range(len(self.mixture_weights))):
             if ploidy > max_ploidy_of_interest:
                 break
@@ -236,60 +292,81 @@ if __name__ == "__main__":
     from collections import defaultdict
     import matplotlib.pyplot as plt
     
+#    ###################################
+#    # Generate some semi-realistic data
+#    ###################################
+#    
+#    # the simulation parameters
+#    N = 100000
+#    var_inflation = 1.5
+#    max_cov = 255
+#    hap_cov = 25
+#    err_frac = .01
+#    hap_frac = .95
+#    dip_frac = .01
+#    high_copy_total_frac = .03
+#    err_scale = 1.0
+#    high_copy_fracs = [exp(-cov / hap_cov) for cov in range(3 * hap_cov, 2 * max_cov, hap_cov)]
+#    Z = sum(high_copy_fracs)
+#    for i in range(len(high_copy_fracs)):
+#        high_copy_fracs[i] *= high_copy_total_frac / Z
+#    mixture_fracs = [hap_frac, dip_frac] + high_copy_fracs
+#    # do the simulation
+#    counts = defaultdict(int)
+#    for i in range(round(err_frac * N)):
+#        x = int(expovariate(err_scale) + 1)
+#        if x <= max_cov:
+#            counts[x] += 1
+#    for j in range(len(mixture_fracs)):
+#        for i in range(round(mixture_fracs[j] * N)):
+#            x = max(1, int(round(normalvariate((j + 1) * hap_cov, sqrt((j + 1) * var_inflation * hap_cov)))))
+#            if x <= max_cov:
+#                counts[x] += 1
+#                
+    
     ###################################
-    # Generate some semi-realistic data
+    # Load data
     ###################################
     
-    # the simulation parameters
-    N = 100000
-    var_inflation = 1.5
-    max_cov = 255
-    hap_cov = 25
-    err_frac = .01
-    hap_frac = .95
-    dip_frac = .01
-    high_copy_total_frac = .03
-    err_scale = 1.0
-    high_copy_fracs = [exp(-cov / hap_cov) for cov in range(3 * hap_cov, 2 * max_cov, hap_cov)]
-    Z = sum(high_copy_fracs)
-    for i in range(len(high_copy_fracs)):
-        high_copy_fracs[i] *= high_copy_total_frac / Z
-    mixture_fracs = [hap_frac, dip_frac] + high_copy_fracs
-    # do the simulation
-    counts = defaultdict(int)
-    for i in range(round(err_frac * N)):
-        x = int(expovariate(err_scale) + 1)
-        if x <= max_cov:
-            counts[x] += 1
-    for j in range(len(mixture_fracs)):
-        for i in range(round(mixture_fracs[j] * N)):
-            x = max(1, int(round(normalvariate((j + 1) * hap_cov, sqrt((j + 1) * var_inflation * hap_cov)))))
-            if x <= max_cov:
-                counts[x] += 1
-                
-    
+    counts_fp = "HG01358.paternal.ont.sorted.counts"
+    counts = {}
+    with open(counts_fp) as f:
+        for line in f:
+            cov, count = map(int, line.strip().split())
+            counts[cov] = count
+            
+            
+    covs = sorted(counts)[:200]
+    plt.plot(covs, [counts[c] for c in covs])
+        
     ###################################
     # Fit the model
     ###################################
     
-    cov_model = CoverageDistribution.fit(counts, tol = 1e-4, max_iters=1000)
+    cov_model = CoverageDistribution.fit(counts, tol = 1e-2, max_iters=100)
+
+    hap_cov = int(CoverageDistribution.heuristic_fit(counts).cov_per_ploidy)
 
     # look at how good the fit is:
-    plt.plot(sorted(counts), [counts[f] for f in sorted(counts)])
-    X = list(range(1, max_cov))
-    Y = [cov_model.pdf(x) for x in X]
-    scale_factor = counts[hap_cov] / Y[hap_cov]
+    covs = sorted(counts)[:min(200, len(counts))]
+    plt.plot(covs, [counts[f] for f in covs])
+    Y = [cov_model.pdf(x) for x in covs]
+    scale_factor = counts[hap_cov] / Y[covs.index(hap_cov)]
     Y = [scale_factor * y for y in Y]
-    plt.plot(X, Y)
-    plt.ylim(top = 10000)
+    plt.plot(covs, Y)
+    plt.show()
     
     ###################################
     # Query some probabilities
     ###################################
     
-    for cov in (10, 20, 30, 40, 50, 60, 70, 80, 90, 100):
+    for cov in range(0, 50, 5):
         prob_err = cov_model.probability_erroneous(cov)
         prob_correct = cov_model.probability_haploid(cov)
         prob_collapsed = cov_model.probability_collapsed(cov)
         
-        print("k-mer frequency {}: prob error {:.4f}, hap {:.4f}, coll {:.4f},".format(cov, prob_err, prob_correct, prob_collapsed))
+        print("coverage level {}: prob error {:.4f}, hap {:.4f}, coll {:.4f},".format(cov, prob_err, prob_correct, prob_collapsed))
+    
+
+        
+            
