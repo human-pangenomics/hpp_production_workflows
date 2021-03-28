80f8b83ea67d158d35557834a021e2dc9af63608
Mobin Asri <masri@ucsc.edu>
2021-03-28 23:02:55 +0000

    Modified calc_mean_sd.py to get .counts instead of .cov

--- asset/docker/asset/scripts/calc_mean_sd.py
+++ asset/docker/asset/scripts/calc_mean_sd.py
@@ -5,49 +5,39 @@ import math
 
 def main():
     parser = argparse.ArgumentParser(description='Get the coverage file and calculate mean and sd')
-    parser.add_argument('--coverageInput', type=str,
-                    help='Input coverage file')
+    parser.add_argument('--countsInput', type=str,
+                    help='Input counts file')
     parser.add_argument('--meanOutput', type=str,
                     help='File path to save coverage mean')
     parser.add_argument('--sdOutput', type=str,
                     help='File path to save coverage standard deviation')
     args = parser.parse_args()
-    coveragePath = args.coverageInput
+    countsPath = args.countsInput
     meanOutputPath = args.meanOutput
     sdOutputPath = args.sdOutput
 
     # Calculate the mean
     totalSize = 0
     totalCoverage = 0
-    with open(coveragePath,'r') as f:
+    with open(countsPath,'r') as f:
         for line in f:
-            if line.startswith(">"):
-                continue
             attrbs = line.strip().split()
-            start = int(attrbs[0])
-            end = int(attrbs[1])
-            coverage = int(attrbs[2])
-            size = (end - start + 1)
-            totalSize += size
-            totalCoverage += coverage * size
+            coverage = int(attrbs[0])
+            count = int(attrbs[1])
+            totalCoverage += coverage * count
+            totalSize += count
     coverageMean = totalCoverage / totalSize
 
     # Calculate the standard deviation
-    totalSize = 0
     totalSquaredCoverage = 0
-    with open(coveragePath,'r') as f:
+    with open(countsPath,'r') as f:
         for line in f:
-            if line.startswith(">"):
-                continue
             attrbs = line.strip().split()
-            start = int(attrbs[0])
-            end = int(attrbs[1])
-            coverage = int(attrbs[2])
-            size = (end - start + 1)
+            coverage = int(attrbs[0])
+            count = int(attrbs[1])
             # Exclude very large coverages in calculating standard deviation
             if (coverage < (2.5 * coverageMean)):
-                totalSize += size
-                totalSquaredCoverage += ((coverage - coverageMean) ** 2) * size
+                totalSquaredCoverage += ((coverage - coverageMean) ** 2) * count
     coverageSD = math.sqrt(totalSquaredCoverage/totalSize)
 
     with open(meanOutputPath,'w') as f:
